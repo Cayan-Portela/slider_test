80d9f4594ffed4cdc125972001662b98cc5ff5cd
Cayan-Portela <cayanportela@hotmail.com>
2017-12-18 13:21:55 -0200

    Update server.R

--- server.R
+++ server.R
@@ -2,6 +2,15 @@ library(shiny)
 
 function(input, output, session) {
   
+   output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles(
+        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>%
+      setView(lng = -93.85, lat = 37.45, zoom = 4)
+  })
+  
 output$valor_slider <- renderPrint({ input$slider })
 
 }
